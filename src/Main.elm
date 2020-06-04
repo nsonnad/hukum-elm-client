bcdd2149f49ba3e55a54c260f200efeb1ccdf1b8
Nikhil Sonnad <nikhil@nsnnd.com>
2020-06-04 21:00:49 +0530

    getting some ports set up and using presence to view players

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,54 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, button, div, h1, h2, input, li, p, text, ul)
+import Html.Attributes exposing (class, classList, placeholder, type_, value)
+import Html.Events exposing (onClick, onInput)
+import Json.Decode as JD
+import Json.Encode as JE
+
+
+type Status
+    = Unregistered
+    | InLobby
+
+
+port addNewUser : JE.Value -> Cmd msg
+
+
+port registered : (JE.Value -> msg) -> Sub msg
+
+
+port gotUserList : (JE.Value -> msg) -> Sub msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ registered Registered
+        , gotUserList GotUserList
+        ]
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { status : Status
+    , user_name : String
+    , all_users : List String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { status = Unregistered
+      , user_name = ""
+      , all_users = []
+      }
+    , Cmd.none
+    )
 
 
 
@@ -22,12 +56,46 @@ init =
 
 
 type Msg
-    = NoOp
+    = UpdateUsername String
+    | AddNewUser JE.Value
+    | Registered JE.Value
+    | GotUserList JE.Value
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        UpdateUsername user_name ->
+            ( { model | user_name = user_name }, Cmd.none )
+
+        AddNewUser value ->
+            ( model, addNewUser value )
+
+        Registered status ->
+            case JD.decodeValue JD.bool status of
+                Ok True ->
+                    ( { model | status = InLobby }
+                    , Cmd.none
+                    )
+
+                Ok False ->
+                    ( model, Cmd.none )
+
+                Err message ->
+                    ( model, Cmd.none )
+
+        GotUserList usersRaw ->
+            case JD.decodeValue decodeUserList usersRaw of
+                Ok users ->
+                    ( { model | all_users = users }, Cmd.none )
+
+                Err error ->
+                    ( model, Cmd.none )
+
+
+decodeUserList : JD.Decoder (List String)
+decodeUserList =
+    JD.list JD.string
 
 
 
@@ -36,10 +104,45 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+    case model.status of
+        Unregistered ->
+            div [ class "new-player-form" ]
+                [ p [] [ text "Username:" ]
+                , input
+                    [ type_ "text"
+                    , placeholder "username"
+                    , value model.user_name
+                    , onInput UpdateUsername
+                    ]
+                    []
+                , viewAddNewUserButton model
+                ]
+
+        InLobby ->
+            div []
+                [ h1 [] [ text "In tha Lobby!" ]
+                , h2 [] [ text "Current players:" ]
+                , ul [] (List.map viewUserList model.all_users)
+                ]
+
+
+viewUserList : String -> Html Msg
+viewUserList user =
+    li [] [ text user ]
+
+
+viewAddNewUserButton : Model -> Html Msg
+viewAddNewUserButton model =
+    let
+        broadcastNewUser =
+            model.user_name
+                |> JE.string
+                |> AddNewUser
+                |> onClick
+    in
+    button
+        [ type_ "submit", broadcastNewUser ]
+        [ text "Submit" ]
 
 
 
@@ -52,5 +155,5 @@ main =
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
-import { Socket } from 'phoenix';
+import { Socket, Presence } from 'phoenix';
 
 let socket = new Socket("ws://localhost:4000/socket", {})
 socket.connect()
 
-let channel = socket.channel("game:lobby", {})
-channel.join()
-  .receive("ok", resp => {
-    console.log('joined game channel', resp)
-  })
-
-Elm.Main.init({
+var app = Elm.Main.init({
   node: document.getElementById('root')
 });
+
+app.ports.addNewUser.subscribe(function(user_name) {
+  let channel = socket.channel("game:lobby", {user_name: user_name})
+
+  channel.join()
+    .receive("ok", resp => { app.ports.registered.send(true) })
+    .receive("error", resp => { app.ports.registered.send(false) })
+
+  let presence = new Presence(channel)
+  let presences = {}
+
+  channel.on("presence_diff", state => {
+    presences = Presence.syncDiff(presences, state)
+    app.ports.gotUserList.send(Object.keys(presences))
+  })
+
+  channel.on("presence_state", state => {
+    presences = Presence.syncState(presences, state)
+    app.ports.gotUserList.send(Object.keys(presences))
+  })
+
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
